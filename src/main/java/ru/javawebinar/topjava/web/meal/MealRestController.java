9b1af001422e5e4742267d9a3c0b593b1c546e37
Gon4iyKot <tsapko.vladimir@gmail.com>
2019-06-18 23:28:23 +0400

    HW2.4: layers for food

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,47 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-public class MealRestController {
-    private MealService service;
+import java.util.Collection;
 
+@Controller
+public class MealRestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final MealService service;
+
+    @Autowired
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal create(Meal meal, int userId) {
+        log.info("create", meal);
+        return service.create(meal, userId);
+    }
+
+    public Meal get(int id, int userId) {
+        log.info("get", id);
+        return service.get(id, userId);
+    }
+
+    public void delete(int id, int userId) {
+        log.info("delete", id);
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal, int userId) {
+        log.info("update", meal);
+        service.update(meal, userId);
+    }
+
+    public Collection<Meal> getAll(int userId) {
+        log.info("getall", userId);
+        return service.getAll(userId);
+    }
 }

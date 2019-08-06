6b1efd872a07046e20a47b18ca682239c9c0ecb2
Gon4iyKot <tsapko.vladimir@gmail.com>
2019-08-07 00:03:58 +0400

    Revert "HW8.1-8.2: ajax for meals"
    This reverts commit fe59f4e4

--- src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatters.java
+++ src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatters.java
@@ -1,10 +1,8 @@
 package ru.javawebinar.topjava.web.converter;
 
 import org.springframework.format.Formatter;
-import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -38,18 +36,4 @@ public class DateTimeFormatters {
             return lt.format(DateTimeFormatter.ISO_LOCAL_TIME);
         }
     }
-
-    public static class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
-
-        @Override
-        public LocalDateTime parse(String text, Locale locale) {
-            return StringUtils.isEmpty(text) ? null : LocalDateTime.parse(text);
-        }
-
-        @Override
-        public String print(LocalDateTime lt, Locale locale) {
-            return lt.format(DateTimeFormatter.ISO_LOCAL_TIME);
-        }
-    }
-
 }
